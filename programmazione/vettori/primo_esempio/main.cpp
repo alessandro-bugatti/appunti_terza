9f798d56624353045687f80e15bf1ca594dffc01
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-01-08 08:53:02 +0100

    Primo esempio sui vettori

---
+++ programmazione/vettori/primo_esempio/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[6]; //Esempio del libro
+    int precipitazioni[12];//Precipitazioni mensili
+    v[0] = 8;
+    v[4] = 5;
+    for (int i = 0; i < 6; i++)
+        cout << v[i] << endl;
+    return 0;
+}
