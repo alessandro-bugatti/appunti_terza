c1049960c660d27a5fb10652c953559f5fc15120
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-01-27 12:19:02 +0100

    Aggiunge la funzione per l'eliminazione

--- programmazione/vettori/eliminazione/main.cpp
+++ programmazione/vettori/eliminazione/main.cpp
@@ -11,10 +11,32 @@ stampa_vettore(int v[], int n)
         cout << "posizione " << i << " => " << v[i] << endl;
 }
 
+//Funzione per l'eliminazione di un elemento
+//Prima versione con il passaggio per riferimento
+//del numero di elementi
+void elimina(int v[], int &n, int posizione)
+{
+    for (int i=posizione; i<n; i++)
+        v[i]=v[i+1];
+    n--;
+}
+
+//Funzione per l'eliminazione di un elemento
+//Seconda versione con il ritorno
+//del numero di elementi
+/*int elimina(int v[], int n, int posizione)
+{
+    for (int i=posizione; i<n; i++)
+        v[i]=v[i+1];
+    n--;
+    return n;
+}*/
+
+
 int main()
 {
     //srand(time(NULL));
-    const int DIMENSIONE = 20;
+    const int DIMENSIONE = 10;
     int vettore[DIMENSIONE];
     int quanti = DIMENSIONE;
     for (int i=0; i < quanti; i++)
@@ -31,10 +53,11 @@ int main()
         cin >> posizione;
     }while (posizione>quanti);
 
-    for (int i=posizione; i<quanti; i++)
-        vettore[i]=vettore[i+1];
+    //Come va chiamata la prima versione
+    elimina(vettore, quanti, posizione);
 
-    quanti--;
+    //Come va chiamata la seconda versione
+    //quanti = elimina(vettore, quanti, posizione);
 
     cout << "vettore:" << endl;
     stampa_vettore(vettore, quanti);
