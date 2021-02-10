a49105c800a57f9556d121bb1645b8c25706447e
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-02-10 09:55:19 +0100

    Aggiunto l'esercizio dell'area vulcanica, da completare

---
+++ programmazione/matrici/es_31/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+const int N_SENSORI = 10;
+const int GIORNI = 30;
+
+int main()
+{
+    int scelto;
+    float temperature[N_SENSORI][GIORNI];
+    for (int i = 0; i < N_SENSORI; i++)
+        for (int j = 0; j < GIORNI; j++)
+            temperature[i][j] = rand()%100;
+    cout << "Scegli un sensore";
+    cin >> scelto;
+    cout << "Il valori letti dal sensore " << scelto << " sono :" << endl;
+    for (int i = 0; i < GIORNI; i++)
+        cout << "Il giorno " << i + 1 << " la temperatura era : " << temperature[scelto][i] << endl;
+    return 0;
+}
