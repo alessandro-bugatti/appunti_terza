a8a299ed87a6ba6ec85c6e9b5831d1e7a17fd83a
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-02-22 13:14:17 +0100

    Aggiornato un esercizio

--- programmazione/correttore/rete/main.cpp
+++ programmazione/correttore/rete/main.cpp
@@ -7,9 +7,22 @@ const int MAX_DIM = 100;
 int min_somma_riga(int m[][MAX_DIM], int R, int C)
 {
    int minimo = 0;
+   int indice_minimo = 0;
    for (int i = 0; i < C; i++)
         minimo += m[0][i];
-
+   for (int i = 0; i < R; i++){
+        int somma = 0;
+        for (int j = 0; j < C; j++)
+        {
+            somma += m[i][j];
+        }
+        if (somma < minimo)
+        {
+            minimo = somma;
+            indice_minimo = i;
+        }
+    }
+    return indice_minimo;
 }
 
 int main()
