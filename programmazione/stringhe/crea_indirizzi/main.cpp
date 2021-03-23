dd26ea98724231c1e21b87c2450e6bd87a1b0faf
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-03-23 10:00:17 +0100

    Esercizio sulla creazione degli indirizzi email

---
+++ programmazione/stringhe/crea_indirizzi/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+string crea_email(string csv)
+{
+    string nome, cognome;
+    //Algoritmo di estrazione delle informazioni
+    //Utilizzo di find per trovare ;
+    int separatore = csv.find(";");
+
+    //Utilizzo substr per estrarre il nome e il cognome
+    cognome = csv.substr(0, separatore);
+    nome = csv.substr(separatore + 1);
+    return nome + "." + cognome + "@itiscastelli.it";
+}
+
+int main()
+{
+    string csv = "ARRIVETTI;ALBERTO";
+
+    //Output: ALBERTO.ARRIVETTI@itiscastelli.it
+    cout << crea_email(csv);
+    return 0;
+}
