2bec2732bab3f49e191b39e8ac10a33b40d432a5
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-04-12 12:22:17 +0200

    Aggiorna il programma del cifrario per sostituzione

--- programmazione/stringhe/cifrario_sostituzione/main.cpp
+++ programmazione/stringhe/cifrario_sostituzione/main.cpp
@@ -7,9 +7,29 @@ char conversione(char c, string chiave)
     return chiave.at(c - 'a');
 }
 
+
+string converti(string chiaro, string chiave)
+{
+    string risultato;
+    for (int i = 0; i < chiaro.size(); i++)
+        risultato += conversione(chiaro.at(i), chiave);
+    return risultato;
+}
+
+string decodifica(string cifrato, string chiave)
+{
+    string risultato;
+    for (int i = 0; i < cifrato.size(); i++)
+        risultato += chiave.find(cifrato.at(i)) + 'a';
+    return risultato;
+}
+
+
 int main()
 {
-    string chiave = "wqertyuiopasdfghjklzxcvbnm";
+    string chiave = "pasdfghjklwqertyuiozxcvbnm";
+    //cout << "Inserisci la chiave di cifratura:";
+    //cin >> chiave;
 
     /*
     Idea di partenza
@@ -23,6 +43,12 @@ int main()
     if (s.at(i) == 'a')
         s.at(i) = 'r';
     ecc. ecc.*/
-    cout << conversione('b', chiave)<< endl;
+    string chiaro = "buongiorno";
+    string cifrato = converti(chiaro, chiave);
+    string decodificato = decodifica(cifrato, chiave);
+    cout << "La stringa in chiaro era " << chiaro << endl;
+    cout << "La stringa codificata diventa " << cifrato << endl;
+    cout << "La stringa decodificata diventa " << decodificato << endl;
+
     return 0;
 }
