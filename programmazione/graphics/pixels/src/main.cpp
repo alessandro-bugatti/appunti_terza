19e23e523e7d5b2851b4557c6787ed6d274a31c7
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2021-12-13 14:31:35 +0100

    Aggiunge il primo esempio di utilizzo di Raylib

---
+++ programmazione/graphics/pixels/src/main.cpp
@@ -0,0 +1,76 @@
+
+
+/*******************************************************************************************
+*
+*   raylib [core] example - Basic window
+*
+*   Welcome to raylib!
+*
+*   To test examples, just press F6 and execute raylib_compile_execute script
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+
+void DisegnaPunto(int x, int y, int dim, Color c)
+{
+    for (int i = 0; i < dim ; ++i) {
+        for (int j = 0; j < dim ; ++j) {
+            DrawPixel(x+i, y+j,c);
+        }
+    }
+}
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1024;
+    const int screenHeight = 768;
+
+    InitWindow(screenWidth, screenHeight, "Pixels, a first example");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        //DrawText("Congrats! You created your first window!", 190, 200, 40, RED);
+
+        //DrawPixel(100, 200, BLUE);
+        DisegnaPunto(100,200,5,BLUE);
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
