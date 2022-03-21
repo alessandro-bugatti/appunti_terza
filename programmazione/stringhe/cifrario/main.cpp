99dc131aae7724c692408488b6ca52ff6f8e3592
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-03-21 18:01:42 +0100

    Aggiunge nuove forme di cifratura

--- programmazione/stringhe/cifrario/main.cpp
+++ programmazione/stringhe/cifrario/main.cpp
@@ -47,6 +47,8 @@ int scrivi_stringhe(const string& nomefile, string s[], int n)
     return 1 ;
 }
 
+
+
 string codifica_uno(string chiaro)
 {
     string codificata;
@@ -66,13 +68,54 @@ string codifica_uno(string chiaro)
     return codificata;
 }
 
+string codifica_cesare(string chiaro, int n)
+{
+    string codificata;
+    for (int i = 0; i < chiaro.size(); ++i) {
+        char c = chiaro.at(i);
+        if ((c >= 'a' && c <= 'z') )
+            codificata += (((c + n) - 'a') % 26) + 'a';
+        else if(c >= 'A' && c <= 'Z')
+            codificata += (((c + n) - 'A') % 26) + 'A';
+        else
+            codificata += c;
+    }
+    return codificata;
+}
+
+
+string codifica_sostituzione(string chiaro, string chiave)
+{
+    string codificata;
+    string chiaveMaiuscola;
+    for (int i = 0; i < chiave.size(); ++i) {
+        chiaveMaiuscola += toupper(chiave.at(i));
+    }
+    for (int i = 0; i < chiaro.size(); ++i) {
+        char c = chiaro.at(i);
+        if ((c >= 'a' && c <= 'z') )
+            codificata += chiave.at(chiaro.at(i) - 'a');
+        else if(c >= 'A' && c <= 'Z')
+            codificata += chiaveMaiuscola.at(chiaro.at(i) - 'A');
+        else
+            codificata += c;
+    }
+    return codificata;
+}
+
+
 int main() {
     const int N_STRINGHE = 100;
     string v[N_STRINGHE];
+    string chiave = "qwertyuioplkjhgfdsazxcvbnm";
     int n_righe = leggi_stringhe("chiaro.txt", v);
     for (int i = 0; i < n_righe; ++i) {
-        v[i] = codifica_uno(v[i]);
+        //v[i] = codifica_cesare(v[i], 8);
+        v[i] = codifica_sostituzione(v[i], chiave);
     }
     scrivi_stringhe("cifrato.txt", v, n_righe);
+//    char c = 'a';
+//    c = (((c + 7) - 'a') % 26) + 'a';
+//    cout << c << endl;
     return 0;
 }
