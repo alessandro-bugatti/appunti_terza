b67ee2de9d7fd68dd9f75f694d8f50acd98421ef
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-04-22 17:14:17 +0200

    Aggiorna l'esercizio sull'autosalone aggiungendo la gestione dei file

--- programmazione/strutture_file/es_11/main.cpp
+++ programmazione/strutture_file/es_11/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -14,9 +15,91 @@ struct Auto{
     Acquirente acquirente;
 };
 
+/**
+ * Funzione che prende una riga estratta dal file dove sono memorizzate
+ * le macchine, la scompone nei suoi campi e restituisce la struttura
+ * composta con quei valori
+ * @param riga Stringa CSV contenente i dati di un'auto
+ * @return La struttura Automobile riempita con tutti i dati
+ */
+Auto elabora_auto(string riga){
+    Auto macchina;
+    //Estrae la marca, che si trova prima del primo punto e virgola
+    int pos = riga.find(';');
+    macchina.marca = riga.substr(0,pos);
+    //Estrae la cilindrata e la trasforma in un intero
+    int pos_prec = pos;
+    pos = riga.find(';',pos_prec + 1);
+    string temp;
+    temp = riga.substr(pos_prec + 1, pos - pos_prec - 1);
+    macchina.cilindrata = stoi(temp);
+    //Estrae l'anno di immatricolazione e lo trasforma in un intero
+    pos_prec = pos;
+    pos = riga.find(';',pos_prec + 1);
+    temp = riga.substr(pos_prec + 1, pos - pos_prec - 1);
+    macchina.immatricolazione = stoi(temp);
+    //Estrae il nome dell'acquirente
+    pos_prec = pos;
+    pos = riga.find(';',pos_prec + 1);
+    macchina.acquirente.nome = riga.substr(pos_prec + 1, pos - pos_prec - 1);
+    //Estrae il cognome dell'acquirente, dall'ultimo punto e virgola fino alla fine
+    macchina.acquirente.cognome = riga.substr(pos + 1);
+    return macchina;
+}
+
+//Tracciato del file
+//marca;cilindrata;immatricolazione;nome;cognome
+/**
+ * Funzione per caricare le macchine dal file
+ * @param automobili Vettore dove verranno inserite le macchine
+ * @param filename Nome del file delle macchine
+ * @return Numero di macchine caricate, -1 se l'apertura non va a buon fine
+ */
+int carica_file(Auto automobili[], string filename)
+{
+    ifstream in(filename);
+    if (!in)
+        return -1;
+    string temp;
+    int contatore = 0;
+    while(getline(in, temp)){
+        Auto macchina = elabora_auto(temp);
+        automobili[contatore] = macchina;
+        contatore++;
+    }
+    in.close();
+    return contatore;
+}
+
+/**
+ * Funzione che salva il vettore delle macchine su file
+ * @param automobili Vettore delle macchine
+ * @param n_auto Numero di macchine presenti nel vettore
+ * @param filename File dove salvare le macchine
+ * @return 0 successo, -1 problemi nell'apertura
+ */
+int salva_file(Auto automobili[], int n_auto, string filename )
+{
+    ofstream out(filename);
+    if (!out)
+        return -1;
+    for (int i = 0; i < n_auto; ++i) {
+        out << automobili[i].marca << ";"
+        << automobili[i].cilindrata << ";"
+        << automobili[i].immatricolazione << ";"
+        << automobili[i].acquirente.nome << ";"
+        << automobili[i].acquirente.cognome << endl;
+    }
+    out.close();
+    return 0;
+}
+
 void stampa_auto(Auto a)
 {
-    cout << a.marca << " " << a.acquirente.cognome << endl;
+    cout << a.marca << " "<< a.cilindrata
+    << " " << a.immatricolazione << " "
+    << a.acquirente.nome << " "
+    << a.acquirente.cognome << endl;
 }
 
 void stampa_autosalone(Auto macchine[], int vendute) {
@@ -105,12 +188,12 @@ void visualizza_macchine_anno(Auto macchine[], int vendute, int anno)
 }
 
 int main() {
-    const int MACCHINE = 10;
+    const int MACCHINE = 1000;
     Auto autosalone[MACCHINE];
-    int macchine_vendute = inizializza_esempi(autosalone);
+    int macchine_vendute = 0;// = inizializza_esempi(autosalone);
     //Esempio di sintassi di come caricare tutti i dati in una
     //struttura in un "colpo solo"
-    Auto esempio = {
+    /*Auto esempio = {
             "Ferrari",
             2500,
             2011,
@@ -127,7 +210,12 @@ int main() {
     esempio2.immatricolazione = 2014;
     esempio2.acquirente.nome = "Anna";
     esempio2.acquirente.cognome = "Bislenghi";
-
+    */
+    macchine_vendute = carica_file(autosalone, "automobili.txt");
+    if (macchine_vendute == -1) {
+        cout << "Non è stato trovato il file delle automobili" << endl;
+        macchine_vendute = 0;
+    }
     //Qui inizia il programma vero e proprio
     int scelta;
     menu();
@@ -159,6 +247,7 @@ int main() {
         cout << "Inserisci la scelta: ";
         cin >> scelta;
     }
+    salva_file(autosalone, macchine_vendute, "automobili.txt");
     return 0;
 }
 
