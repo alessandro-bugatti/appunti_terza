7c84a89ef443fc076da050ffc985dd6f4587e6b6
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-12-09 10:51:31 +0100

    Prime versione dell'elevamento a potenza intera

--- funzioni/primo_esempio/main.cpp
+++ funzioni/primo_esempio/main.cpp
@@ -18,11 +18,36 @@ int potenza_di_due(int n) //Firma della funzione
     return risultato;
 }
 
+/**
+ * Funzione che eleva la base a un esponente intero
+ * @param base
+ * @param esp Deve essere intero
+ * @return base elevata a esponente
+ */
+float potenza_intera(float base, int esp)//Parametri formali
+{
+    float risultato = 1;
+    for (int i = 0; i < esp; ++i) {
+        risultato *= base;
+    }
+    return risultato;
+}
+
+
 int main() {
     int potenza, esponente;
     std::cout << "Inserisci l'esponente intero positivo: ";
     std::cin >> esponente;
     potenza = potenza_di_due(esponente); //Chiamata di funzione
-    std::cout << "2 elevato alla " << esponente << " da come risultato " << potenza;
+    std::cout << "2 elevato alla " << esponente << " da come risultato " << potenza << std::endl;
+    std::cout << "Inserisci la base: ";
+    float base;
+    std::cin >> base;
+    std::cout << base << " elevato alla " << esponente << " da come risultato " <<
+        potenza_intera(base, esponente); //parametri attuali
+    //Attenzione bene: i nomi dei parametri non contano, conta solo la posizione
+    //std::cout << base << " elevato alla " << esponente << " da come risultato " <<
+    //    potenza_intera(esponente, base); //Così non fa quello che vogliamo!
+
     return 0;
 }
