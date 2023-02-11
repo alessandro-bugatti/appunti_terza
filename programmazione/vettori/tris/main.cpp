56f9cfd2e4eb97e8b8ee004becf2abdb35507251
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2023-02-11 11:00:18 +0100

    Aggiorna il gioco del tris

--- programmazione/vettori/tris/main.cpp
+++ programmazione/vettori/tris/main.cpp
@@ -6,7 +6,7 @@ char caratteri(int c){
     if (c == 0)
         return ' ';
     if (c == 1)
-        return '*';
+        return 'X';
     return 'O';
 }
 
@@ -31,6 +31,57 @@ void stampa(int tris[][N]){
     }
 }
 
+/**
+ * Funzione che inserisce una mossa, se valida
+ * @param tris La matrice del tris
+ * @param r la riga dove voglio inserire, prima riga 0
+ * @param c la colonna dove voglio inserire, prima colonna 0
+ * @param giocatore 1 o 2
+ * @return true se la mossa è valida, e quindi viene fatta, false altrimenti
+ */
+bool fai_mossa(int tris[][N],int r, int c, int giocatore){
+    if (r < 0 || r > 2)
+        return false;
+    if (c < 0 || c > 2)
+        return false;
+    if (tris[r][c] != 0)
+        return false;
+    tris[r][c] = giocatore;
+    return true;
+}
+
+/**
+ *
+ * @param tris
+ * @return 1 se vince il giocatore 1
+ *  2 se vince il giocatore 2
+ *  3 patta
+ *  0 partita in corso
+ */
+int verifica_stato(int tris[][N]){
+    //controllo delle righe
+    for (int i = 0; i < N; ++i) {
+        if(tris[i][0] == tris[i][1] && tris[i][0] == tris[i][2])
+            return tris[i][0];
+    }
+    //controllo delle colonne
+    for (int i = 0; i < N; ++i) {
+        if(tris[0][i] == tris[1][i] && tris[0][i] == tris[2][i])
+            return tris[0][i];
+    }
+    if (tris[0][0] == tris[1][1] && tris[1][1] == tris[2][2])
+        return tris[1][1];
+    if (tris[0][2] == tris[1][1] && tris[1][1] == tris[2][0])
+        return tris[1][1];
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (tris[i][j] == 0)
+                return 0;
+        }
+    }
+    return 3;
+}
+
 int main() {
 
     //0 casella vuota
@@ -43,9 +94,41 @@ int main() {
             tris[i][j] = 0;
         }
     }
-    tris[0][1] = 1;
-    tris[1][0] = 2;
+    //tris[0][1] = 1;
+    //tris[1][0] = 2;
     stampa(tris);
+
+    int giocatore = 1;
+    int r, c;
+    while(verifica_stato(tris) == 0){
+        //Input del giocatore corrente
+        std::cout << "Giocatore " << giocatore << " fai la tua mossa: " << std::endl;
+        std::cout << "Inserisci la riga (1-3): ";
+        std::cin >> r;
+        std::cout << "Inserisci la colonna (1-3): ";
+        std::cin >> c;
+        //Verifica della mossa e eventuale ripetizione dell'input
+        while(!fai_mossa(tris, r - 1, c - 1, giocatore)){
+            std::cout << "Attenzione " << giocatore << " hai sbagliato, rifai la tua mossa: " << std::endl;
+            std::cout << "Inserisci la riga (1-3): ";
+            std::cin >> r;
+            std::cout << "Inserisci la colonna (1-3): ";
+            std::cin >> c;
+        }
+        stampa(tris);
+        //cambio turno
+        if (giocatore == 1)
+            giocatore = 2;
+        else
+            giocatore = 1;
+    }
+    int risultato = verifica_stato(tris);
+    if (risultato == 3){
+        std::cout << "Patta" << std::endl;
+    }
+    else {
+        std::cout << "Ha vinto il giocatore " << risultato << std::endl;
+    }
 
     return 0;
 }
