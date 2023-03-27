5a09d8f7228511202282954b8fbca272cab31fb1
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2023-03-27 16:44:56 +0200

    Scheletro funzionante dell'applicazione grafica per indovinare una parola.

--- programmazione/grafica/attenti_al_mostro/main.cpp
+++ programmazione/grafica/attenti_al_mostro/main.cpp
@@ -40,41 +40,100 @@ int main(void)
     const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Attenti al mostro");
 
-    std::string segreta = "cancello";
+    std::string segreta = ""; //"cancello";
     std::string visibile;
 
-    for (int i = 0; i < segreta.size(); ++i) {
-        visibile += '_';
-    }
+
 
     int errori = 0;
+
+    int game_state;
+
+    const int INIZIO = 0;
+    const int SCELTA_PAROLA = 1;
+    const int INDOVINA = 2;
+    const int RIGIOCA = 3;
 
     Texture2D tex = LoadTexture("../mind_flayer.png");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-
+    game_state = INIZIO;
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+
+        char key = GetCharPressed();
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        switch (game_state) {
+            case INIZIO:
+                DrawText("Attenti al mostro", 20, 100, 30, BLACK);
+                DrawText("Premi un tasto per continuare", 20, 150, 30, BLACK);
+                if (key > 0)
+                    game_state = SCELTA_PAROLA;
+                break;
+            case SCELTA_PAROLA:
+                DrawText("Inserisci la parola da indovinare", 20, 100, 30, BLACK);
+                DrawText(segreta.c_str(), 20, 150, 30, BLACK);
+                if (key > 0)
+                    segreta += key;
+                if (IsKeyPressed(KEY_BACKSPACE))
+                    segreta = segreta.substr(0, segreta.size() - 1);
+                if (IsKeyPressed(KEY_ENTER)) {
+                    game_state = INDOVINA;
+                    for (int i = 0; i < segreta.size(); ++i) {
+                        visibile += '_';
+                    }
+                }
+                break;
+            case INDOVINA:
+                while(key > 0){
+                    if (ricerca(segreta, key) == false)
+                        errori++;
+                    else
+                        svela(visibile, key, segreta);
+                    std::cout << errori << std::endl;
+                    key = GetCharPressed();
+                }
+                disegna(tex, errori);
+                DrawText(visibile.c_str(),20, screenHeight - 60, 60,BLACK);
+                if (errori == 9 || visibile == segreta)
+                    game_state = RIGIOCA;
+                break;
+            case RIGIOCA:
+                if (errori == 9) {
+                    DrawText("Hai perso", 20, 100, 60, BLACK);
+                    std::string messaggio = "La parola da indovinare era " + segreta;
+                    DrawText(messaggio.c_str(), 20, 180, 30, BLACK);
+                }
+                else{
+                    DrawText("Hai vinto", 20, 100, 60, BLACK);
+                }
+                DrawText("Vuoi continuare (y/n)?", 20, 300, 30, BLACK);
+                if (key > 0) {
+                    if (key == 'y'){
+                        game_state = SCELTA_PAROLA;
+                        segreta = visibile = "";
+                        errori = 0;
+                    }
+                    else{
+                        CloseWindow();
+                        return 0;
+                    }
+
+                }
+                break;
+        }
+
         // Update
         //----------------------------------------------------------------------------------
-        char key = GetCharPressed();
-        while(key > 0){
-            if (ricerca(segreta, key) == false)
-                errori++;
-            else
-                svela(visibile, key, segreta);
-            std::cout << errori << std::endl;
-            key = GetCharPressed();
-        }
+
+
 
         // Draw
         //----------------------------------------------------------------------------------
-        BeginDrawing();
-        ClearBackground(WHITE);
-        disegna(tex, errori);
-        DrawText(visibile.c_str(),20, screenHeight - 60, 60,BLACK);
 
 
         EndDrawing();
