75332443d8d2ae50c54dcee7f6e7b9215327215c
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2023-05-12 11:01:16 +0200

    Aggiunge nuove modalità al primo esempio di utilizzo di file di testo

--- programmazione/strutture/file_primo_esempio/main.cpp
+++ programmazione/strutture/file_primo_esempio/main.cpp
@@ -7,7 +7,7 @@ int main() {
     //1) Apertura in lettura (equivalente a cin) o in scrittura (cout)
     //Esempio in lettura
 
-    std::ifstream in("dati.txt");  //Apertura di un file in lettura
+    std::ifstream in("../dati.txt");  //Apertura di un file in lettura
 
     if(!in){
         std::cout << "Apertura fallita" << std::endl;
@@ -21,6 +21,57 @@ int main() {
     //3) chiusura del file
     in.close();
 
+    //Esempio di lettura di un file contenente esattamente 2 numeri interi
+    int n1, n2;
+    std::ifstream in2("../dati2.txt");
+    if(!in2){
+        std::cout << "Apertura fallita" << std::endl;
+        return 1;
+    }
+    in2 >> n1;
+    in2 >> n2;
 
+    std::cout << "Il prodotto dei due numeri vale " << n1*n2 << std::endl;
+    in2.close();
+
+    std::ifstream in3("../dati3.txt");
+    if (!in3){
+        std::cout << "Apertura fallita" << std::endl;
+        return 1;
+    }
+    int numero;
+    std::string stringa;
+
+    in3 >> numero >> stringa;
+
+    for (int i = 0; i < numero; ++i) {
+        std::cout << stringa << std::endl;
+    }
+    in3.close();
+
+    //Esempio con tanti valori
+    //PRima versione: all'inizio del file c'è scritto quanti sono
+    int N, temp;
+    std::ifstream in4("../elenco_con_n.txt");
+    if (!in4){
+        std::cout << "Apertura fallita" << std::endl;
+        return 1;
+    }
+    in4 >> N;
+    for (int i = 0; i < N; ++i) {
+        in4 >> temp;
+        std::cout << temp << std::endl;
+    }
+    //Seconda versione: leggi finchè ce ne sono
+    std::ifstream in5("../elenco.txt");
+    if (!in5){
+        std::cout << "Apertura fallita" << std::endl;
+        return 1;
+    }
+    float valore;
+    while(in5 >> valore){
+        std::cout << valore << std::endl;
+    }
+    in5.close();
     return 0;
 }
