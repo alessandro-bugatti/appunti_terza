640280e41b6ebef6a0573ae6d7ee6c3e410fe124
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2024-02-19 11:51:48 +0100

    Gioco del tris, da completare

---
+++ programmazione/vettori/tris/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+int main() {
+    const int DIM = 3;
+    int tris[DIM][DIM];
+    for (int i = 0; i < DIM; ++i) {
+        for (int j = 0; j < DIM; ++j) {
+            tris[i][j] = 0;
+        }
+    }
+
+
+    return 0;
+}
