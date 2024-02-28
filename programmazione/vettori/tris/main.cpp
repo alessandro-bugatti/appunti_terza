76e1eefa23384ac8e9e681b522baf0fa0581768a
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2024-02-28 17:58:01 +0100

    Aggiunge pezzi al tris, da completare

--- programmazione/vettori/tris/main.cpp
+++ programmazione/vettori/tris/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+const int DIM = 3;
 
+void stampa_tris(int tris[][DIM], int n){
+    for (int i = 0; i < n; ++i) {
+        std::cout << tris[i][0] << " | " << tris[i][1]
+                  << " | " << tris[i][2] << std::endl;
+        if (i < DIM - 1)
+            std::cout << "________________" << std::endl;
+    }
+}
 
 int main() {
-    const int DIM = 3;
+
     int tris[DIM][DIM];
     for (int i = 0; i < DIM; ++i) {
         for (int j = 0; j < DIM; ++j) {
@@ -11,6 +20,22 @@ int main() {
         }
     }
 
+    stampa_tris(tris, DIM);
+
+    int r, c;
+    std::cin >> r >> c;
+    while ( r > DIM || r < 1 ||
+                c > DIM || c < 1
+            || tris[r-1][c-1] != 0){
+        std::cin >> r >> c;
+    }
+
+    int vittoria = 0;
+    for (int i = 0; i < DIM; ++i) {
+        if (tris[i][0] != 0 && tris[i][0] == tris[i][1]
+            && tris[i][1] == tris[i][2])
+            vittoria = tris[i][0];
+    }
 
     return 0;
 }
