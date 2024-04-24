c65734a6dca39ceaeec54852b27571ccd5b2a2d7
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2024-04-24 16:02:31 +0200

    Aggiunge le strutture dati

--- programmazione/file/persistenza/main.cpp
+++ programmazione/file/persistenza/main.cpp
@@ -2,6 +2,27 @@
 #include <sstream>
 #include <vector>
 
+struct Data{
+    int giorno;
+    int mese;
+    int anno;
+};
+
+struct Valutazione {
+    float voto;
+    std::string materia;
+    Data data;
+};
+
+struct Studente {
+    std::string nome;
+    std::string cognome;
+    Data nascita;
+    int classe;
+    char sezione;
+    std::vector <Valutazione> valutazioni;
+};
+
 std::vector<std::string> split(const std::string& s, char delimiter = ' ')
 {
     std::stringstream in(s);
