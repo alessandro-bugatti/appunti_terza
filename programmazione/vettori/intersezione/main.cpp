4ed6368ab23f2581a68330767444042869913bee
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-01-29 16:14:22 +0100

    Esempio di unione e intersezione, da completare

---
+++ programmazione/vettori/intersezione/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+void riempi(int v[], int n) {
+    for (int i = 0; i < n; ++i) {
+        v[i] = rand() % 20;
+    }
+}
+
+void stampa(int v[], int n) {
+    for (int i = 0; i < n; ++i) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void stampa(const std::vector<int> &v) {
+    for (int i = 0; i < v.size(); ++i) {
+        std::cout << v.at(i) << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+bool presente (const std::vector<int> &v, int c) {
+    for (int i = 0; i < v.size(); ++i) {
+        if (v.at(i) == c)
+            return true;
+    }
+    return false;
+}
+
+void unisci(int src[], int n, std::vector<int> &dst) {
+    for (int i = 0; i < n; ++i) {
+        if (!presente(dst, src[i])) {
+            dst.push_back(src[i]);
+        }
+    }
+}
+
+int main() {
+    const int DIM = 10;
+    int v1[DIM], v2[DIM];
+    std::vector<int> unione, intersezione;
+    riempi(v1, DIM);
+    riempi(v2, DIM);
+    stampa(v1, DIM);
+    stampa(v2, DIM);
+    unisci(v1, DIM, unione);
+    unisci(v2, DIM, unione);
+    stampa(unione);
+    return 0;
+}
