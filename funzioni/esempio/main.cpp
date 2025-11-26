77483224f45a55faf05f71d2216c1b428660d074
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-11-26 08:52:03 +0100

    Aggiunge i commenti all'esempio

--- funzioni/esempio/main.cpp
+++ funzioni/esempio/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <cmath>
 
+/**
+ * Calcola l'ipotenusa dati i due cateti
+ * @param c1 Primo cateto
+ * @param c2 Secondo cateto
+ * @return Valore dell'ipotenusa
+ */
 double ipotenusa(double c1, double c2) {
     double risultato = sqrt(c1*c1 + c2*c2);
     return risultato;
